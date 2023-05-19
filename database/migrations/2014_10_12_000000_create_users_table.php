8ee35e935ff3e3d2259d98a8986920202a674d57
David Alejandro Ramírez Marín <davidalejo_14@hotmail.com>
2023-05-19 12:33:50 -0500

    Configuración inicial
    se configura login y register y CSS

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,24 +8,35 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('tipe_document');
+            $table->string('document_id');
+            $table->string('celular');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password');            
             $table->rememberToken();
+            $table->foreignId('rol_id')->nullable();
+            $table->foreignId('concesionary_nit')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+            $table->boolean('active');
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
